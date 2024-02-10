02e6e4bf45b9919b6ad75215b63abaf9b929e7ca
Xinqi Mu <hi@muxinqi.com>
2024-02-10 19:59:13 +0800

    Setup shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button.tsx'
+
 function App() {
-  return <>nodecho-web</>
+  return (
+    <>
+      <p>nodecho-web</p>
+      <Button onClick={() => alert('Hello')}>Click Me</Button>
+    </>
+  )
 }
 
 export default App
